33c9b8069bc9b9625bdda2f17d47341450c6341a
github-action-benchmark <github@users.noreply.github.com>
2026-03-24 23:01:05 +0000

    add Fallow Binary Size (customSmallerIsBetter) benchmark result for 5d4e06f89e7bb8da14741c36b1f5490ceb5b30ba

--- dev/bloat/data.js
+++ dev/bloat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774392934937,
+  "lastUpdate": 1774393265490,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Binary Size": [
@@ -226,6 +226,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/fallow-rs/fallow/commit/9cfedd96d8195004b83382bd3cf7f33d664c7c8b"
         },
         "date": 1774392934538,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size (fallow)",
+            "value": 116272400,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "5d4e06f89e7bb8da14741c36b1f5490ceb5b30ba",
+          "message": "chore: apply cargo fmt to test code from refactoring agents",
+          "timestamp": "2026-03-24T23:58:24+01:00",
+          "tree_id": "22eb7bda7c26dbb7a4490be0e478e6454a3e7ff9",
+          "url": "https://github.com/fallow-rs/fallow/commit/5d4e06f89e7bb8da14741c36b1f5490ceb5b30ba"
+        },
+        "date": 1774393265131,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
